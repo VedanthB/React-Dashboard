3ee9bd21ea46bd560c7899b55c0d762b82e30be0
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-05-28 17:46:04 +0530

    [FEAT]: added homepage/ ecommerce page

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import "./App.css";
+
 import { ContextProvider } from "./context";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <ContextProvider>
       <App />
     </ContextProvider>
   </React.StrictMode>,
+  document.getElementById("root"),
 );
