f2c9b702913d9d669b6ef5c36223db737cd9c850
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-05-28 10:09:45 +0530

    [FEAT]: eslint + prettier + husky

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import './App.css';
+import "./App.css";
 
 const App = () => {
-  return (
-    <div className="bg-gray-500 p-10 text-center">
-      hello world
-    </div>
-  );
-}
+  return <div className="bg-gray-500 p-10 text-center">hello world</div>;
+};
 
 export default App;
